9560e0758819dd7ab78c42e83e4a997a5a8a8280
chrisokoth <chrisokoth17@gmail.com>
2023-12-17 03:04:59 -0500

    decalring a variable

---
+++ cpp_programming/1-variable.cpp
@@ -0,0 +1,20 @@
+// A program that declares 2 variables and stores them
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+//Decalre the first variable
+int num =10;
+//declare the 2nd variable
+string name = "Chris";
+//third variable
+char x = 'B';
+//Output the variables in different lines
+cout<<" Your name is: "<<name;
+cout<<"\n Your declared number is: "<<num;
+cout<<"\n And your favourite character is: "<<x;
+return 0;
+
+
+}
