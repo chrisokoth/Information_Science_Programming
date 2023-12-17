50f36848a4093118bac76891a505ffcd3768f052
chrisokoth <chrisokoth17@gmail.com>
2023-12-17 17:47:22 +0300

    I love you 10 times

---
+++ cpp_programming/1-while_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// A program that outputs I love you until the condition is satisfied
+
+int main(){
+int i=0;
+while (i<=10){
+cout<<"I love you!\n";
+i++;
+}
+return 0;
+
+
+}
