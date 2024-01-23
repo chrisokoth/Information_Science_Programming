88e9c4460dbcd94ca58d14196577d4b16bc4e172
chrisokoth <chrisokoth17@gmail.com>
2024-01-23 11:34:03 +0300

    function syntax

--- cpp_programming/cpp_functions/function1.cpp
+++ cpp_programming/cpp_functions/function1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-
 void myFunction(){
-// block of code
-/* myFunction is the name of the function
-void means that the function does not return any value
-*/
-
+cout<<"I was executed!";
 
 }
+int main(){
+myFunction();
+return 0;
+
+}
+
