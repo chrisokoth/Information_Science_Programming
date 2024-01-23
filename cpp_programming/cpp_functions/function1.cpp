591547cd88ca230a3696b02ff798926bb0f30764
chrisokoth <chrisokoth17@gmail.com>
2024-01-23 11:17:01 +0300

    functions syntax

---
+++ cpp_programming/cpp_functions/function1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(){
+// block of code
+/* myFunction is the name of the function
+void means that the function does not return any value
+*/
+
+
+}
